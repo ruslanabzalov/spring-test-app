17785bdb1d2e770fac639a56b825403c20cc4a88
Ruslan Abzalov <rabzalov@t1-consulting.ru>
2022-04-11 23:21:58 +0300

    Add initial commit

---
+++ spring-test-app-backend/src/main/java/abzalov/ruslan/springtestappbackend/SpringTestAppBackendApplication.java
@@ -0,0 +1,13 @@
+package abzalov.ruslan.springtestappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestAppBackendApplication.class, args);
+    }
+
+}
